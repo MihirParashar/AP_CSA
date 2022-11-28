e1a7d944df56875e703b8cd7b58793b38fcb34f1
meeruTheCoder <mihir.parashar1@gmail.com>
2022-11-27 19:15:25 -0600

    Added notes for Algorithms

---
+++ src/Algorithms.java
@@ -0,0 +1,14 @@
+public class Algorithms {
+    /*
+    ALGORITHM: A set of detailed steps to reach a certain goal. It is written steps, not code.
+    IMPLEMENTATION: Implementing an algorithm through a programming language, such as Java.
+    Time Complexity: The amount of time an algorithm takes to complete depending on its inputs.
+    Space Complexity: The amount of space an algorithm requires depending on its inputs.
+    Big-O notation: A mathematical notation that describes how time and space complexity scales
+    as the input approaches infinity. CONSTANTS ARE IGNORED!
+
+    O(1) (Constant Time): Time to complete stays the same regardless of input.
+    O(n) (Linear Time): Time scales as the input increases.
+    O(n^2) (Quadratic Time): Time scales squared as the input increases.
+     */
+}
